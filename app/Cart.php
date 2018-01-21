52c378517d1cd6898408a0d25d2d089f0b8b304f
devuros <urosjovanovic0704@gmail.com>
2018-01-21 20:26:21 +0100

    Cart eloquent model add scope

--- app/Cart.php
+++ app/Cart.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
 
-    //
+	/**
+	 * Get the games in cart of the requested user
+	 */
+    public function scopeUser($query, $user)
+    {
+
+    	return $query->where('user_id', $user);
+
+    }
+
 
 }
