7717cf5aa7130a23d70b354756f56f734f1a187d
devuros <urosjovanovic0704@gmail.com>
2018-01-21 20:15:27 +0100

    FinishGameController

--- app/Game.php
+++ app/Game.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use Carbon\Carbon;
+
 class Game extends Model
 {
 
@@ -54,22 +56,45 @@ class Game extends Model
     }
 
     /**
-     *
+     * Get games which are on sale
      */
-    public function underFive()
+    public function scopeOnSale($query)
     {
 
-        //
+        return $query->where('is_on_sale', '1');
 
     }
 
     /**
-     *
+     * Get games which are priced under ten
      */
-    public function underTen()
+    public function scopeUnderTen($query)
     {
 
-        //
+        return $query->where('sale_price', '<', '10');
+
+    }
+
+    /**
+     * Get games which are priced under ten
+     */
+    public function scopeUnderFive($query)
+    {
+
+        return $query->where('sale_price', '<', '5');
+
+    }
+
+    /**
+     * Get games which are one month old
+     */
+    public function scopeNewRelease($query)
+    {
+
+        $now = Carbon::now();
+        $month = Carbon::now()->subMonths(1);
+
+        return $query->whereBetween('release_date', [$month, $now]);
 
     }
 
