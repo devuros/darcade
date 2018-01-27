3bf024cd3f7814b22f56c440757d71b124a79ccf
devuros <urosjovanovic0704@gmail.com>
2018-01-27 14:32:08 +0100

    Try x OrdersTableSeeder

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 class OrdersTableSeeder extends BaseSeeder
 {
+
     /**
      * Seed tables: orders, purchases, game_user
      */
@@ -18,87 +19,90 @@ class OrdersTableSeeder extends BaseSeeder
 
         $purchases = $this->getPurchasesPerOrder();
 
-        foreach (range(1, $users) as $user)
+        $timestamp = Carbon\Carbon::now();
+
+        DB::transaction(function () use ($users, $games, $orders, $purchases, $timestamp)
         {
 
-            // number of orders that will be seeded for the user
-
-            $user_orders = array_random($orders);
-
-            if ($user_orders != 0)
+            foreach (range(1, $users) as $user)
             {
 
-                // number of purchases that will be seeded for the order
+                // number of orders that will be seeded for the user
 
-                $order_purchases = array_random($purchases);
+                $user_orders = array_random($orders);
 
-                foreach (range(1, $order_purchases) as $index)
+                if ($user_orders == 0)
+                {
+                    break;
+                }
+
+                foreach (range(1, $user_orders) as $order_index)
                 {
 
-                    //
+                    // number of purchases that will be seeded for the order
+
+                    $order_purchases = array_random($purchases);
+
+                    $total = 0;
+
+                    $order_id = DB::table('orders')->insertGetId([
+
+                        'user_id'=> $user,
+                        'total'=> $total,
+                        'created_at'=> $timestamp,
+                        'updated_at'=> $timestamp,
+
+                    ]);
+
+                    foreach (range(1, $order_purchases) as $purchase_index)
+                    {
+
+                        // game that will be seeded in purchases and library
+
+                        $random_game = array_random($games);
+
+                        // get game details
+
+                        $game = App\Game::find($random_game);
+
+                        $actual_price = $game->base_price;
+
+                        if ($game->is_on_sale)
+                        {
+
+                            $actual_price = $game->sale_price;
+
+                        }
+
+                        $total += $actual_price;
+
+                        DB::table('purchases')->insert([
+
+                            'game_id'=> $random_game,
+                            'order_id'=> $order_id,
+                            'actual_price'=> $actual_price,
+
+                        ]);
+
+                        DB::table('game_user')->insert([
+
+                            'game_id'=> $random_game,
+                            'user_id'=> $user,
+                            'created_at'=> $timestamp,
+                            'updated_at'=> $timestamp,
+
+                        ]);
+
+                    }
+
+                    DB::table('orders')->where('id', $order_id)->update(['total'=> $total]);
 
                 }
 
             }
 
-        }
+        });
 
     }
-
-    // $random_games = array_random($games, rand(1, 3));
-
-    // $time = Carbon\Carbon::now();
-
-    // $order_id = DB::table('orders')->insertGetId([
-
-    //     'user_id'=> $user,
-    //     'total'=> 0,
-    //     'created_at'=> $time,
-    //     'updated_at'=> $time,
-
-    // ]);
-
-    // $total = 0;
-
-    // foreach ($random_games as $random_game)
-    // {
-
-    //     $actual_price = 0;
-
-    //     if ($random_game['is_on_sale'])
-    //     {
-
-    //         $total += $random_game['sale_price'];
-    //         $actual_price = $random_game['sale_price'];
-
-    //     }
-    //     else
-    //     {
-
-    //         $total += $random_game['base_price'];
-    //         $actual_price = $random_game['base_price'];
-
-    //     }
-
-    //     DB::table('purchases')->insert([
-
-    //         'game_id'=> $random_game['id'],
-    //         'order_id'=> $order_id,
-    //         'actual_price'=> $actual_price,
-
-    //     ]);
-
-    //     DB::table('game_user')->insert([
-
-    //         'game_id'=> $random_game['id'],
-    //         'user_id'=> $user,
-    //         'created_at'=> $time,
-    //         'updated_at'=> $time,
-
-    //     ]);
-
-    // }
-
-    // DB::table('orders')->where('id', $order_id)->update(['total'=> $total]);
 
 }
